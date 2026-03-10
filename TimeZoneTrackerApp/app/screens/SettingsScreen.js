71d017730a58e5cd61f1a46911a364d1514a0d80
ihsanyigitbasi92 <ihsanyigitbasi92@gmail.com>
2026-03-10 23:31:04 +0400

    Add TimeZoneTrackerApp/app/screens/SettingsScreen.js

---
+++ TimeZoneTrackerApp/app/screens/SettingsScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const SettingsScreen = () => {
+  return (
+    <View>
+      <Text>Settings</Text>
+      {/* Add settings components here */}
+    </View>
+  );
+};
+
+export default SettingsScreen;
