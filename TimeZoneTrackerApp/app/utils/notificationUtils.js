2366ccc19fa9844a689f7012dc3f6e13da75de49
ihsanyigitbasi92 <ihsanyigitbasi92@gmail.com>
2026-03-10 23:31:08 +0400

    Add TimeZoneTrackerApp/app/utils/notificationUtils.js

---
+++ TimeZoneTrackerApp/app/utils/notificationUtils.js
@@ -0,0 +1,9 @@
+export const scheduleNotification = (timeZone, time) => {
+  // Logic to schedule notification
+  console.log(`Notification scheduled for ${timeZone} at ${time}`);
+};
+
+export const cancelNotification = (timeZone) => {
+  // Logic to cancel notification
+  console.log(`Notification cancelled for ${timeZone}`);
+};
